2dfc0bfc578cb209edf3d8a625dd11ab3f4f65b3
thomas-torp <thomas.torp@schibsted.com>
2018-09-18 09:36:33 +0200

    Initial commit

---
+++ swiftlint-analyze-bug/Spacing.swift
@@ -0,0 +1,7 @@
+
+extension CGFloat {
+    public static var normalSpacing: CGFloat = 8
+    public static func normalSpacing(multiplier: CGFloat) -> CGFloat {
+        return normalSpacing * multiplier
+    }
+}
